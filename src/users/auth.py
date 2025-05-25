f2b432aa3132f2618b08322ae891f09582d7fea8
Rafal Szadkowski <badgersdev@gmail.com>
2025-05-25 11:46:06 +0200

    add auth.py and use cookie-based JWT auth

---
+++ src/users/auth.py
@@ -0,0 +1,21 @@
+from ninja_jwt.authentication import JWTAuth
+from ninja_jwt.tokens import UntypedToken
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from jwt import decode as jwt_decode, InvalidTokenError
+
+
+class JWTAuthFromCookie(JWTAuth):
+    def authenticate(self, request, token=None):
+        token = request.COOKIES.get("auth-token")
+        if not token:
+            return None
+
+        try:
+            UntypedToken(token)
+            decoded = jwt_decode(
+                token, settings.SECRET_KEY, algorithms=["HS256"])
+            user = get_user_model().objects.get(id=decoded["user_id"])
+            return user
+        except (InvalidTokenError, get_user_model().DoesNotExist):
+            return None
